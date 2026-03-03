3e9c889bdcde0387b43f1510a9f61feb8ee17d18
D-JPM <declanjpmetcalfe@protonmail.com>
2026-03-03 20:01:07 +0000

    Implement task acceptance from user, store in 'tasks' dictionary

--- cli-todo/main.py
+++ cli-todo/main.py
@@ -15,11 +15,11 @@ def main():
         if user_input == "quit":
             print("You have closed the App")
             break
-        # Acknowledge users command
-        print(f"commond type recognised: {user_input}")
-
-
-
+        # if the command is 'add'
+        elif user_input == "add":
+            task_name = input("Enter task name: ") # Ask for user task and store in var
+            tasks[task_name] = False # Add that task to the tasks dict as the key
+            print(f"the task {task_name} as been added to your Todo list")
 
 if __name__ == "__main__":
     main()
