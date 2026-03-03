c974aac5002021917bff30916d02c96affcb319e
D-JPM <declanjpmetcalfe@protonmail.com>
2026-03-03 19:48:29 +0000

    implement core application loop and task dictionary

--- cli-todo/main.py
+++ cli-todo/main.py
@@ -1,5 +1,24 @@
+
 def main():
-    print("Hello from cli-todo!")
+
+    # Dict to store users tasks
+    tasks = {}
+
+
+    # program persistance
+    while True:
+        # Print instruction
+        print("type 'quit' to end app")
+        # Get user task
+        user_input = input("Enter command type: ")
+        # Check user input is quit
+        if user_input == "quit":
+            print("You have closed the App")
+            break
+        # Acknowledge users command
+        print(f"commond type recognised: {user_input}")
+
+
 
 
 if __name__ == "__main__":
