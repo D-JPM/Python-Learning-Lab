adf20eb495973f418624b9760a2a717d1bbdab68
D-JPM <declanjpmetcalfe@protonmail.com>
2026-03-08 22:36:51 +0000

    refactor: Implement single-line commands

--- cli-todo/main.py
+++ cli-todo/main.py
@@ -15,17 +15,34 @@ def main():
 
     # program persistance
     while True:
+        
         # Get user task
         user_input = input("Enter command type: ")
+
+        # split words for single line command logic
+        words = user_input.split()
+        
+        # if user inputs nothing 
+        if not words:
+            continue
+        
         # Check user input is quit
         if user_input == "quit":
             print("You have closed the App")
             break
+        
         # if the command is 'add'
-        elif user_input == "add":
-            task_name = input("Enter task name: ") # Ask for user task and store in var
-            tasks[task_name] = False # Add that task to the tasks dict as the key
-            print(f"the task {task_name} as been added to your Todo list")
+        elif words[0] == "add": 
+            # Joine all words back together expect for first words as it's the command
+            task_name = " ".join(words[1:])
+            if not task_name:
+                print("You need to enter a task name")
+            else:
+                tasks[task_name] = False
+                print(f"the task [{task_name}] as been added to your Todo list")
+
+            
+        
         # if the command is 'list'    
         elif user_input == "list":
             if not tasks:
@@ -35,6 +52,7 @@ def main():
                     print(f"{task} Completed")
                 else:
                     print(f"{task} Not completed")
+        
         # if the command is 'done'
         elif user_input == "done":
             task_name = input("Enter task name: ") # Ask for user task and store in var
@@ -43,6 +61,7 @@ def main():
                 print(f"the task {task_name} as been completed")
             else:
                 print(f"the {task_name} is not in your Todo list!")
+        
         # if the command is 'remove'
         elif user_input == "remove":
             task_name = input("Enter task name: ") # Ask for user task and store in var
